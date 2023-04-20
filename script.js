496dafd17b4d7b87ea454faf62ba18a943efcf2f
jorgelopez8 <123724150+jorgelopez8@users.noreply.github.com>
2023-04-20 22:14:43 +0200

    script

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 calcular();
 function calcular() {
-    let num1 = parseInt(prompt("Escribe el primer número:"));
-    let num2 = parseInt(prompt("Escribe el segundo número:"));
-
+    let num1 = parseInt(prompt("Pon tu primer número:"));
+    let num2 = parseInt(prompt("Pon tu segundo número:"));
     sumar(num1, num2);
     restar(num1, num2);
     multiplicar(num1, num2);
     dividir(num1, num2);
 }
-
 function sumar(num1, num2) {
     let resultado = num1 + num2;
     console.log(`La suma es: ${resultado}`);
-    alert("La solución de la suma es: " + suma);
+    alert("El resultado de la suma es: " + suma);
 }
 
 
 function restar(num1, num2) {
     let resultado = num1 - num2;
     console.log(`La resta es: ${resultado}`);
-    alert("La solución de la resta es: " + resta);
+    alert("El resultado de la resta es: " + resta);
 }
 
 function multiplicar(num1, num2) {
     let resultado = num1 * num2;
     console.log(`La multiplicación es: ${resultado}`);
-    alert("La solución de la multiplicación es: " + multiplicación);
+    alert("El resultado de la multiplicación es: " + multiplicación);
 }
 
 
 function dividir(num1, num2) {
     let resultado = num1 / num2;
     console.log(`La división es: ${resultado}`);
-    alert("La solución de la división es: " + división);
+    alert("El resultado de la división es: " + división);
 
 }
