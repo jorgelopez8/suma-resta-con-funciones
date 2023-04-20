55ad2d35b894688270833cf906a6a31a9f94a0f1
jorgelopez8 <123724150+jorgelopez8@users.noreply.github.com>
2023-04-20 22:33:39 +0200

    script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 calcular();
 function calcular() {
-    let num1 = parseInt(prompt("Escribe el primer número:"));
-    let num2 = parseInt(prompt("Escribe el segundo número:"));
+    let num1 = parseInt(prompt("Ingrese el primer número:"));
+    let num2 = parseInt(prompt("Ingrese el segundo número:"));
+
     sumar(num1, num2);
     restar(num1, num2);
     multiplicar(num1, num2);
     dividir(num1, num2);
 }
+
 function sumar(num1, num2) {
     let resultado = num1 + num2;
     console.log(`La suma es: ${resultado}`);
